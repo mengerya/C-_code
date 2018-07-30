1ca0bb38e3daea828c0b0a7bf73929354171ecf6
mengerya <30553694+mengerya@users.noreply.github.com>
2018-07-30 22:44:58 +0800

    Update 末尾0的个数.c

--- niuke/末尾0的个数.c
+++ niuke/末尾0的个数.c
@@ -7,7 +7,17 @@ int main(){
 	int n = 0;
 	int count = 0;
 	int flag = 0;
-	cin >> n;
+	scanf("%d", &n);
+	if (n<0)
+		n = n*(-1);
+	if (n == 0){
+		cout << 1 << endl;
+		return 0;
+	}
+	if (n < 5 && n>0){
+		cout << 0 << endl;
+		return 0;
+	}
 	for (size_t i = 5; i <= n; i += 5){
 		flag = 0;
 		for (size_t j = i; j >= 5; j /= 5){
